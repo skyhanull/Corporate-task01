8a4df2fce5dade09abc4763b8c1e6bd4c6fbca44
skyhanull <anky0425@naver.com>
2023-03-15 21:57:11 +0900

    feat: store생성

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { ChakraProvider } from '@chakra-ui/react'
+import { store } from './store/store'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ChakraProvider>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ChakraProvider>
+  </Provider>
 )
