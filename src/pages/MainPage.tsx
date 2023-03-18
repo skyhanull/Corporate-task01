42c14a3ebb9d8fb480b477118bd915535293d692
skyhanull <anky0425@naver.com>
2023-03-18 21:15:47 +0900

    refactor: main코드 map으로 리스트 뿌림

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,5 +1,39 @@
+import { useSelector } from 'react-redux'
+import TripFitler from '../components/Card/TripFilter'
+import TripCard from '../components/Card/TripCards'
+import { RootState } from '../store/store'
+
 function MainPage() {
-  return <div>a</div>
+  const tripFilter = useSelector((state: RootState) => state.Triplist)
+  console.log(tripFilter)
+  return (
+    <div>
+      <TripFitler />
+      <div
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+          flexWrap: 'wrap',
+          gap: '10px',
+        }}
+      >
+        {tripFilter.result.map(list => (
+          <TripCard
+            key={list.idx}
+            idx={list.idx}
+            name={list.name}
+            mainImage={list.mainImage}
+            description={list.description}
+            spaceCategory={list.spaceCategory}
+            price={list.price}
+            maximumPurchases={list.maximumPurchases}
+            registrationDate={list.registrationDate}
+          />
+        ))}
+      </div>
+      {/* <TripCard /> */}
+    </div>
+  )
 }
 
 export default MainPage
