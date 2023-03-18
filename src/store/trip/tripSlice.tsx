8ef3185dc0d6497043c38edda7d276dfbe267414
skyhanull <anky0425@naver.com>
2023-03-19 03:14:32 +0900

    feat: 지역이름 필터부분 slice로 옮김

---
+++ src/store/trip/tripSlice.tsx
@@ -0,0 +1,41 @@
+import { createSlice, current } from '@reduxjs/toolkit'
+import { useSelector } from 'react-redux'
+import ITravelInfo from '../../types'
+import { RootState } from '../store'
+import tripApi from './tripThunk'
+import { tripFilter } from '../../util/util'
+import { it } from 'node:test'
+
+// const tripFilter = useSelector((state: RootState) => state.Triplist)
+// console.log(tripFilter)
+const result: ITravelInfo[] = []
+const itemfilter: ITravelInfo[] = []
+export const tripSlice = createSlice({
+  name: 'tripSlice',
+  initialState: { result, itemfilter },
+  reducers: {
+    setlist: (state, action) => {
+      state.result = action.payload
+      console.log(state.result)
+    },
+    resetlist: (state, action) => {
+      state.result = action.payload
+      console.log(state.result)
+    },
+    filterspace: (state, action) => {
+      console.log(action.payload)
+      const A = state.result.filter(el => el.spaceCategory === action.payload)
+      state.itemfilter = [...A, ...state.itemfilter]
+    },
+  },
+  extraReducers: builder => {
+    builder.addCase(tripApi.fulfilled, (state, action) => {
+      console.log(action)
+      state.result = action.payload
+    })
+  },
+})
+
+export const { setlist, filterspace } = tripSlice.actions
+// store에서 add, remove, complte 액션을 내보낸다.
+export default tripSlice.reducer
