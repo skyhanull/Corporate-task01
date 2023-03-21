b488b252c5ea40cd331b45469d47bd63ec4f4d8d
skyhanull <anky0425@naver.com>
2023-03-21 23:51:14 +0900

    add: slice type변경

--- src/store/trip/tripSlice.tsx
+++ src/store/trip/tripSlice.tsx
@@ -8,6 +8,22 @@ import { it } from 'node:test'
 
 // const tripFilter = useSelector((state: RootState) => state.Triplist)
 // console.log(tripFilter)
+
+export interface ITripState {
+  readonly result: ITravelInfo[]
+  readonly itemfilter: ITravelInfo[]
+  readonly priceRange: ITravelInfo[]
+  // readonly selectedtripList: ITripInfo | null
+}
+
+interface Iactionprice {
+  pre: string
+  next: string
+}
+// interface ISliceType:ITravelInfo[] {
+//   result: [],
+
+// }
 const result: ITravelInfo[] = []
 const itemfilter: ITravelInfo[] = []
 export const tripSlice = createSlice({
@@ -27,6 +43,11 @@ export const tripSlice = createSlice({
       const A = state.result.filter(el => el.spaceCategory === action.payload)
       state.itemfilter = [...A, ...state.itemfilter]
     },
+    filterprice: (state, action) => {
+      console.log(action.payload)
+      const A = state.result.filter(el => el.price === action.payload)
+      // state.itemfilter = [...A, ...state.itemfilter]
+    },
   },
   extraReducers: builder => {
     builder.addCase(tripApi.fulfilled, (state, action) => {
@@ -36,6 +57,6 @@ export const tripSlice = createSlice({
   },
 })
 
-export const { setlist, filterspace } = tripSlice.actions
+export const { setlist, filterspace, filterprice } = tripSlice.actions
 // store에서 add, remove, complte 액션을 내보낸다.
 export default tripSlice.reducer
