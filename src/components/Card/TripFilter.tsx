8819e4bfb07ab487967e536396f19a4e30cf53f7
skyhanull <anky0425@naver.com>
2023-03-26 21:53:34 +0900

    add: useeffect로 url에 params넣음

--- src/components/Card/TripFilter.tsx
+++ src/components/Card/TripFilter.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Link, useParams, useLocation, useSearchParams } from 'react-router-dom'
 import {
@@ -10,54 +10,121 @@ import {
 } from '@chakra-ui/react'
 import { RootState } from '../../store/store'
 import {
-  setlist,
   filterspace,
   filterprice,
   filterpricedd,
 } from '../../store/trip/tripSlice'
+import useUrlHandler from '../../util/util'
 
 interface Iprice {
   pre: string
   next: string
 }
 function TripCard() {
+  const { urlSearchHandler } = useUrlHandler()
   const { name, id } = useParams()
   const [searchParams, setSearchParams] = useSearchParams()
   const [spaceName, setSpaceName] = useState<string[]>([])
+  // const spaceName = []
+  const [nameSpaceparam, setNameSpaceParam] = useState('')
   const [price, setPrice] = useState<string[]>([])
   const dispatch = useDispatch()
 
   const tripFilter = useSelector((state: RootState) => state.Triplist)
-  const AAA = tripFilter.result.map((el: any) => el.spaceCategory)
-  console.log(tripFilter.itemfilter)
+
   const category = ['강원', '서울', '부산', '대구', '제주']
 
-  const iniparams = searchParams.get('name')
+  // const AAAAA = () => {
+  //   const iniparams = searchParams.get('name')
+  //   const c = spaceName.join().trim()
+  //   // console.log(c)
+  //   if (iniparams === null) {
+  //     searchParams.set('name', nameSpaceparam)
+  //     setSearchParams(searchParams)
+  //   } else {
+  //     searchParams.set('name', c)
+  //     setSearchParams(searchParams)
+  //   }
+  // }
+  // const spaceArray = useCallback(
+  //   (namek: string) => {
+  //     // if (!spaceName.includes(namek)) {
+  //     //   setNameSpaceParam(namek)
+  //     //   // spaceName.push(namek)
+  //     // }
 
-  const spaceArray = (namek: string) => {
-    if (!spaceName.includes(namek)) {
-      spaceName.push(namek)
+  //     setSpaceName(prev => [...prev, namek])
+  //     // spaceName.push(nameSpaceparam)
+  //     // // const c = spaceName.join().trim()
+  //     // const A = [...spaceName, namek]
+  //     AAAAA(namek)
+  //   },
+  //   [AAAAA]
+  // )
+  // const spaceArray = nameanme => {
+  //   // if (!spaceName.includes(namek)) {
+  //   //   setNameSpaceParam(namek)
+  //   //   // spaceName.push(namek)
+  //   // }
+  //   SpaceHander(nameanme)
+  //   urlSearchHandler(spaceName)
+
+  //   // spaceName.push(nameSpaceparam)
+  //   // // const c = spaceName.join().trim()
+  //   // const A = [...spaceName, namek]
+  //   // AAAAA(namek)
+  // }
+
+  const SpaceHander = (names: string) => {
+    if (!spaceName.includes(names)) {
+      setNameSpaceParam(names)
+      // spaceName.push(namek)
+      setSpaceName(prev => [...prev, names])
     }
-    dispatch(filterspace(name))
-    const c = spaceName.join().trim()
-    if (iniparams === null) {
-      searchParams.set('name', namek)
-      setSearchParams(searchParams)
-    } else {
-      setSearchParams({ name: c })
-    }
+    // console.log(spaceName)
+    // urlSearchHandler(spaceName)
   }
 
+  // if (spaceName.length < 5) {
+  //   urlSearchHandler(spaceName)
+  // }
+
+  const spaceArray = useCallback(() => {
+    // if (!spaceName.includes(namek)) {
+    //   setNameSpaceParam(namek)
+    //   // spaceName.push(namek)
+    // }
+
+    urlSearchHandler(spaceArray)
+
+    // spaceName.push(nameSpaceparam)
+    // // const c = spaceName.join().trim()
+    // const A = [...spaceName, namek]
+    // AAAAA(namek)
+  }, [urlSearchHandler])
+  // spaceArray(nameSpaceparam)
+  // spaceArray()
   // const ASDF = (e: any) => {
   //   setPrice(e.map((item: any) => item.toString()))
   // }
 
+  useEffect(() => {
+    const AAAAA = () => {
+      // const iniparams = searchParams.get('name')
+      if (spaceName.length > 0) {
+        const c = spaceName.join().trim()
+        searchParams.set('name', c)
+        setSearchParams(searchParams)
+      }
+    }
+    AAAAA()
+  }, [nameSpaceparam, searchParams, setSearchParams, spaceName])
   const filterPriceHandler = (e: any) => {
     const pricearray: Iprice = {
       pre: e[0],
       next: e[1],
     }
-    dispatch(filterprice(pricearray))
+    // dispatch(filterprice(pricearray))
 
     // const B =
     //   next !== undefined
@@ -69,28 +136,33 @@ function TripCard() {
     // return B
   }
 
-  const submitHandler = () => {
-    // const A = tripFilter.result.filter(x1 =>
-    //   spaceName.length > 0
-    //     ? spaceName.find(x2 => x1.spaceCategory === x2)
-    //     : x1.spaceCategory
-    // )
-    // const Fspace = filterPriceHandler()
-    // const arr2 = A.filter(x1 => Fspace.some(x2 => x1.idx === x2.idx))
-    // dispatch(setlist(arr2))
-    // console.log(arr2)
-    // filterPriceHandler()
-  }
+  const submitHandler = useCallback(() => {
+    const pricearray: Iprice = {
+      pre: price[0],
+      next: price[1],
+    }
+    dispatch(filterspace(spaceName))
+    dispatch(filterprice(pricearray))
+  }, [dispatch, price, spaceName])
+
+  useEffect(() => {
+    submitHandler()
+  }, [submitHandler])
 
   useEffect(() => {
     if (tripFilter.itemfilter.length === 0 && searchParams.get('name')) {
-      const KK = tripFilter.result.filter(x1 =>
-        searchParams
-          .get('name')
-          ?.split(',')
-          .find(x2 => x1.spaceCategory === x2)
-      )
-      dispatch(filterpricedd(KK))
+      const urlNameparams = searchParams.get('name')?.split(',')
+      const urlPriceMinParams = searchParams.get('min')
+      const uurlPriceManParams = searchParams.get('max')
+
+      // const KK = tripFilter.result.filter(x1 =>
+      //   searchParams
+      //     .get('name')
+      //     ?.split(',')
+      //     .find(x2 => x1.spaceCategory === x2)
+      // )
+      // dispatch(filterpricedd(KK))
+      dispatch(filterpricedd(urlNameparams))
     }
   }, [dispatch, searchParams, tripFilter.itemfilter.length, tripFilter.result])
 
@@ -102,7 +174,7 @@ function TripCard() {
         min={0}
         max={30000}
         step={5000}
-        onChange={e => filterPriceHandler(e)}
+        onChange={e => setPrice(e.map((item: any) => item.toString()))}
       >
         <RangeSliderTrack>
           <RangeSliderFilledTrack />
@@ -114,7 +186,9 @@ function TripCard() {
       <div>
         {tripFilter && tripFilter?.result.map(el => el.idx)}
         {category.map(el => (
-          <button onClick={() => spaceArray(el)}>{el}</button>
+          <button type='button' key={el} onClick={() => SpaceHander(el)}>
+            {el}
+          </button>
         ))}
       </div>
       {/* <Link
