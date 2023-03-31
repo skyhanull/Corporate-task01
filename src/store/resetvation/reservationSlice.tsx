dbd2e9653894b7d5ac9f5389c875e1b3e4396330
skyhanull <anky0425@naver.com>
2023-03-31 16:24:05 +0900

    feat: 장바구니 slice구현

--- src/store/resetvation/reservationSlice.tsx
+++ src/store/resetvation/reservationSlice.tsx
@@ -1,37 +1,67 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { access } from 'fs'
 import { useSelector } from 'react-redux'
-import ITravelInfo from '../../types'
-import { RootState } from '../store'
 
-const result: ITravelInfo[] = []
+interface IReservation {
+  idx: number
+  name: string
+  amount: number
+  mainImage: string
+  description: string
+  spaceCategory: string
+  price: number
+  maximumPurchases: number
+  registrationDate: string
+}
+
+const result: IReservation[] = []
 // const tripFilter = useSelector((state: RootState) => state.Triplist)
 export const tripSlice = createSlice({
   name: 'tripSlice',
   initialState: { result },
   reducers: {
     addcartlist: (state, action) => {
-      console.log(action.payload)
-      if (state.result.find(item => item.idx === action.payload.idx)) {
-        return
-      } else {
-        state.result.push(action.payload)
-      }
+      state.result.push({ ...action.payload, amount: 1 })
     },
-    resetlist: (state, action) => {
-      console.log(action.payload)
-      if (state.result.find(item => item.idx !== action.payload.idx)) {
-        state.result = action.payload
-      }
-      // state.result = action.payload
-      console.log(state.result)
+    patchItem: (state, action) => {
+      const patchAmount =
+        action.payload.amount < action.payload.maximumPurchases
+          ? { ...action.payload, amount: action.payload.amount + 1 }
+          : action.payload
+
+      const filterpatchdata = state.result.filter(
+        el => el.idx !== action.payload.idx
+      )
+
+      const A = [...filterpatchdata, patchAmount].sort((a, b) => a.idx - b.idx)
+      state.result = A
     },
-    patch: (state, action) => {
-      state.result = []
-      state.result = action.payload
+    removeItem: (state, action) => {
+      const removeAmount =
+        action.payload.amount >= 1
+          ? { ...action.payload, amount: action.payload.amount - 1 }
+          : state.result
+
+      const filterremovedata = state.result.filter(
+        el => el.idx !== action.payload.idx
+      )
+
+      const removeData =
+        action.payload.amount === 1
+          ? filterremovedata
+          : [...filterremovedata, removeAmount]
+      state.result = removeData
+    },
+    removeList: (state, action) => {
+      const filterremoveList = state.result.filter(
+        el => el.idx !== action.payload.idx
+      )
+      state.result = filterremoveList
     },
   },
 })
 
-export const { addcartlist, patch } = tripSlice.actions
+export const { addcartlist, patchItem, removeItem, removeList } =
+  tripSlice.actions
 // store에서 add, remove, complte 액션을 내보낸다.
 export default tripSlice.reducer
