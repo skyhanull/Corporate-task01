74f0e8ad776108d1265ba10da0cfe0f9a5a36e5d
skyhanull <anky0425@naver.com>
2023-04-02 01:00:32 +0900

    add: 안쓰는 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
 import { BrowserRouter, Route, Routes, Navigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
