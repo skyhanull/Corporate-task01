314ad8c01818d91e1124dd45fa1667683ef37037
skyhanull <anky0425@naver.com>
2023-04-02 01:01:53 +0900

    add:store에 persist추가

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,11 +1,44 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import Triplist from './trip/tripSlice'
 import reservationSlice from './resetvation/reservationSlice'
 
-export const store = configureStore({
-  reducer: { Triplist, reservationSlice },
+const reducers = combineReducers({
+  reservationSlice,
+  Triplist,
 })
 
+const persistConfig = {
+  timeout: 500,
+  key: 'book',
+  storage,
+  whitelist: ['reservationSlice'],
+  blacklist: ['Triplist'],
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+      // }).concat(logger),
+    }),
+})
+export const persistor = persistStore(store)
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 
