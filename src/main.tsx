a19c8ae960cade501bd8c825a9a113c104226cff
skyhanull <anky0425@naver.com>
2023-04-01 22:10:04 +0900

    feat: redux-persist추가

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { ChakraProvider } from '@chakra-ui/react'
-import { store } from './store/store'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
 import App from './App'
 import './index.css'
+import store from './store/store'
 
+const persistor = persistStore(store)
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <ChakraProvider>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
+      <PersistGate loading={null} persistor={persistor}>
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </PersistGate>
     </ChakraProvider>
   </Provider>
 )
